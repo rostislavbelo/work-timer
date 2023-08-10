a2c39ca8c36c1fba3acb62ac4437fdc2e8d1928e
FrontEnd <C:\Users\Work\AppData\Roaming\The Bat!\Google>
2023-08-11 02:41:42 +0300

    добавляеет лайаут, хедер, react-router

---
+++ src/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+// import { Link} from "react-router-dom";
+// import  "react-router-dom";
+
+export function Header() {
+  return (
+    <div className="header">
+        Хедыр
+    </div>
+  )
+}
---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import  './layout.css';
+
+
+interface ILayoutProps {
+  children ?: React.ReactNode;
+}
+
+export function Layout({children}:ILayoutProps) {
+  return (
+    <div className="layout">{children}</div>
+  );
+}
