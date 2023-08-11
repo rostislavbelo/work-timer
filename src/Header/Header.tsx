99385314ccd4dfc2d76a3f598bc6a66cc8f01fa9
FrontEnd <C:\Users\Work\AppData\Roaming\The Bat!\Google>
2023-08-11 03:24:14 +0300

    добавляеет иконки хедера

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./header.css";
+import mainLogo from "../icons/mainLogo.svg"
+import statistics from "../icons/statistics.svg"
 // import { Link} from "react-router-dom";
 // import  "react-router-dom";
 
 export function Header() {
   return (
     <div className="header">
-        Хедыр
+        <img src={mainLogo} className="header__logo" alt="logo" />
+        <img src={statistics} className="header__statistics" alt="statistics" />
     </div>
   )
 }
