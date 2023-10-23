21c5d51a260b081e8037bd4777e674b8151a47e5
FrontEnd <C:\Users\Work\AppData\Roaming\The Bat!\Google>
2023-10-23 19:56:48 +0300

    feat: добавляет фокусинпуту задач. добавляет удаление задачи

--- src/TaskManager/TaskManager.tsx
+++ src/TaskManager/TaskManager.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent} from "react";
+import React, { FormEvent, useEffect, useRef} from "react";
 import "./taskManager.css";
 import { useStore } from "../store";
 import { action } from "mobx";
@@ -6,17 +6,28 @@ import { observer } from "mobx-react-lite";
 import threeDots from "../icons/threeDots.svg"
 
 export const TaskManager = observer(() => {
+
+  const refInput = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (refInput.current === null ) return;
+    refInput.current.focus();
+  }, [refInput]);
+
   const { tasksStore } = useStore();
   const handleSubmit = action((e: FormEvent) => {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
-    const value = formData.get("title")?.toString() || "";
+    let value = formData.get("title")?.toString() || "";
     tasksStore.list = [...tasksStore.list, {
       title: value,
       id: Date.now(),
       count: 1,
       popup: false,
     }];
+    if (refInput.current !== null) {
+      refInput.current.value = '';
+    }
   });
 
   const handlerPopup = action((id:number) => {
@@ -31,10 +42,14 @@ export const TaskManager = observer(() => {
     tasksStore.list = tasksStore.list.map(n => n.id === id && n.count > 1 ? { ...n, count: n.count-1} : n)
   })
 
+  const handlerDelete = action((id:number) => {
+    tasksStore.list.map((n, index) => n.id === id ? {...tasksStore.list.splice(index, 1)} : n);
+  })
+
   return (
     <div className="taskManager">
       <form action="#" onSubmit={handleSubmit}>
-        <input name="title" type="text" placeholder="Название задачи" required maxLength={30}/>
+        <input name="title" type="text" placeholder="Название задачи" required maxLength={30} ref={refInput}/>
         <button className="taskManager__btn-add" type="submit">Добавить</button>
       </form>
       <ul className="taskManager__task-list" >      
@@ -50,7 +65,7 @@ export const TaskManager = observer(() => {
                     <button onClick={()=>handlerPlus(l.id)}>Увеличить</button>
                     <button onClick={()=>handlerMinus(l.id)}>Уменьшить</button>
                     <button>Редактировать</button>
-                    <button>Удалить</button>
+                    <button onClick={()=>handlerDelete(l.id)}>Удалить</button>
                 </div>)}
               </div>
             </li>
